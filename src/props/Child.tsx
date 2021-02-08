af30e06a4ad75ff928f06cddf4d0925dc3d6991a
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-08 12:42:39 +0100

    Annotations with Children

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,13 +1,21 @@
 // describe all different props we expect this component Child = () => {} tp receive
 interface ChildProps {
     color: string;
+    onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+    return <div>
+        {color}
+        <button onClick={onClick}>Click me</button>
+    </div>;
 };
 
-// Defining a function component with typescript
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-    return <div>{color}</div>;
+// Defining a function component that receive children with typescript
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick, children }) => {
+    return <div>
+        {color}
+        {children}
+        <button onClick={onClick}>Click me</button>
+    </div>;
 };
